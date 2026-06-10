1a7f62dacc9ff4b157445562d899ef61c861263f
hugdora <hugdora.edjangul@yahoo.fr>
2026-06-10 10:43:03 +0100

    create IAM role

---
+++ iam/ecs-role/outputs.tf
@@ -0,0 +1,11 @@
+# ECS Task Execution Role 
+output "ecs_task_execution_role_arn" {
+  description = "The ARN of the ECS task execution role"
+  value       = 
+}
+
+# ECS Task Role 
+output "ecs_task_role_arn" {
+  description = "The ARN of the ECS task role"
+  value       = 
+}
