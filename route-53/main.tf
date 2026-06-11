0805cbc9442eef199afb13e48d94e9ade887a1e7
hugdora <hugdora.edjangul@yahoo.fr>
2026-06-11 21:41:52 +0100

    update route53 module

--- route-53/main.tf
+++ route-53/main.tf
@@ -10,7 +10,7 @@ resource "aws_route53_record" "site_domain" {
 
   alias {
     name                   = var.application_load_balancer_dns_name
-    zone_id                = application_load_balancer_zone_id
+    zone_id                = var.application_load_balancer_zone_id
     evaluate_target_health = true
   }
 }
