ca86deda69e908387151f0e15b7047fedc424995
hugdora <hugdora.edjangul@yahoo.fr>
2026-06-11 21:11:31 +0100

    create ecs and route53 module

--- iam/ecs-role/outputs.tf
+++ iam/ecs-role/outputs.tf
@@ -7,5 +7,5 @@ output "ecs_task_execution_role_arn" {
 # ECS Task Role 
 output "ecs_task_role_arn" {
   description = "The ARN of the ECS task role"
-  value       = aws_iam_policy_document.ecs_task_assume_role_policy.arn
+  value       = aws_iam_role.ecs_task_role.arn
 }
---
+++ route-53/main.tf
@@ -0,0 +1,16 @@
+# Route 53 
+data "aws_route53_zone" "hosted_zone" {
+  name = var.domain_name
+}
+
+resource "aws_route53_record" "site_domain" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.record_name
+  type    = "A"
+
+  alias {
+    name                   = var.application_load_balancer_dns_name
+    zone_id                = application_load_balancer_zone_id
+    evaluate_target_health = true
+  }
+}
